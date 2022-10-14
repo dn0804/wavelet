a4139b713f95178786ab6120e89839fa362dfe56
dn0804 <78126917+dn0804@users.noreply.github.com>
2022-10-14 16:50:00 -0700

    Update SimpleSearch.java

--- SimpleSearch.java
+++ SimpleSearch.java
@@ -6,8 +6,8 @@ class Handler implements URLHandler {
     // various requests.
     String keyword;
     int num = 0;
-    int num2 = 0;
-    String[] keyWordlist;
+    //int num2;
+    String[] wordlist = new String[] {"football", "soccer", "basketball"};
     String newKeyword;
     /* (non-Javadoc)
      * @see URLHandler#handleRequest(java.net.URI)
@@ -16,16 +16,17 @@ class Handler implements URLHandler {
         if (url.getPath().equals("/")) {
             return String.format("Simple Search Engine");
 
-        } else if (url.getPath().equals(keyWordlist[0])) {
-            keyword = keyWordlist[0];
+        } else if (url.getPath().equals(wordlist[0])) {
+            keyword = wordlist[0];
             return String.format(keyword);
-        } else if (url.getPath().equals(keyWordlist[1])) {
-            keyword = keyWordlist[1];
+        } else if (url.getPath().equals(wordlist[1])) {
+            keyword = wordlist[1];
             return String.format(keyword);
         } else if (url.getPath().equals("/testword")) {
             keyword = "test";
             return keyword;
-        } else{
+        } 
+        else {
                 System.out.println("Path: " + url.getPath());
                 if (url.getPath().contains("/add")) {
                     String[] parameters = url.getQuery().split("=");
