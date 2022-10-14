39d614a3295a33c0b5ae0dff6dd2107deac83a33
dn0804 <78126917+dn0804@users.noreply.github.com>
2022-10-14 16:39:10 -0700

    Update SimpleSearch.java

--- SimpleSearch.java
+++ SimpleSearch.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.Scanner;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     String keyword;
-    Scanner searchInput = new Scanner(System.in);
     int num = 0;
     int num2 = 0;
     String[] keyWordlist;
@@ -16,7 +14,7 @@ class Handler implements URLHandler {
      */
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Welcome to the Simple Searh Engine!!!");
+            return String.format("Simple Search Engine");
 
         } else if (url.getPath().equals(keyWordlist[0])) {
             keyword = keyWordlist[0];
@@ -24,16 +22,16 @@ class Handler implements URLHandler {
         } else if (url.getPath().equals(keyWordlist[1])) {
             keyword = keyWordlist[1];
             return String.format(keyword);
-        } else if (url.getPath().equals("/dapple")) {
-            keyword = "dapple";
+        } else if (url.getPath().equals("/testword")) {
+            keyword = "test";
             return keyword;
         } else{
                 System.out.println("Path: " + url.getPath());
                 if (url.getPath().contains("/add")) {
                     String[] parameters = url.getQuery().split("=");
-                    if (parameters[num].equals("newKeyword")) {
+                    if (parameters[num].equals(newKeyword)) {
                         newKeyword = String.format(parameters[num+1]);
-                        return String.format("A New Keyword Has Been Added: s%.", parameters[num+1]); 
+                        return String.format("New search added", parameters[num+1]); 
 
                     }
 
